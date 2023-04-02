436c8fc03e20b57a9e452d3957ad6c14d7b432ce
Okyaz Eminaga <okyaz.eminaga@stanford.edu>
2023-04-01 23:34:36 -0700

    Run me

--- WSI/run_exp_svs_w_xml.sh
+++ WSI/run_exp_svs_w_xml.sh
@@ -1,3 +1,7 @@
+echo "INFO: Generate a list for svs files with the corresponding xml annotation files"
 get_file_list.py
+echo "INFO: Read the files and Run the Prediction"
 main_10x_50_50.py
+echo "INFO: Merge the csv output files into a single files"
 merge.py
+echo "INFO: DONE -> EXIST"
